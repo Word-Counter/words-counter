21a2f65ffd5e85e9c37fc578af79fde3d86cb13e
Muhammad Hafiz Azhari <mhafiz.azh@gmail.com>
2023-06-01 08:45:40 -0700

    function file edited
    on the previous code. There is a bug that the functions is not reuseable. So all of the function updated and should be works and reusable right now.

--- functionList.py
+++ functionList.py
@@ -1,13 +1,10 @@
-sentance = str(input("your sentance: \n"))
-
-
-def wordsCounter():
+def wordsCounter(sentance: str) -> str:
     #make new variable and split the sentance with split() function
     listWords = sentance.split()
     wordsCounted = len(listWords)
     return wordsCounted
 
-def vowelCounter():
+def vowelCounter(sentance: str) -> str:
     #make a list to store list of vowel words
     vowels = ["a", "e", "i", "o", "u"]
     #initiate the count program from 0
@@ -17,11 +14,11 @@ def vowelCounter():
             count += 1
     return count
 
-def characterCounter():
+def characterCounter(sentance: str) -> str:
     count = len(sentance)
     return count
 
-def sentanceCounter():
+def sentanceCounter(sentance: str) -> str:
     dot = '.'
     count = 0
     for char in sentance:
@@ -29,21 +26,16 @@ def sentanceCounter():
             count += 1
     return count
 
-def readingTime():
-    words = wordsCounter()
-    readTime = round(words / 200,3)
+def readingTime(sentance: str) -> str:
+    listWords = sentance.split()
+    wordsCounted = len(listWords)
+    readTime = round(wordsCounted / 200,3)
     return readTime
 
-def speakingTime():
-    words = wordsCounter()
-    speakTime = round(words / 135, 3)
+def speakingTime(sentance: str) -> str:
+    listWords = sentance.split()
+    wordsCounted = len(listWords)
+    speakTime = round(wordsCounted / 135, 3)
     return speakTime
 
-
-print("Words counted:", wordsCounter(), )
-print("Vowel counted:", vowelCounter())
-print("character counted:", characterCounter())
-print("sentance counted:", sentanceCounter())
-print("estimate read time:", readingTime(), "minutes")
-print("estimate speaking time:", speakingTime(), "minutes")
 
--- test.py
+++ test.py
@@ -1,1 +1,10 @@
-import function_wordsCounter
+import functionList
+
+sentance = str(input("your sentance: \n"))
+
+print("Words counted:", functionList.wordsCounter(sentance))
+print("Vowel counted:", functionList.vowelCounter(sentance))
+print("character counted:", functionList.characterCounter(sentance))
+print("sentance counted:", functionList.sentanceCounter(sentance))
+print("estimate read time:", functionList.readingTime(sentance), "minutes")
+print("estimate speaking time:", functionList.speakingTime(sentance), "minutes")
