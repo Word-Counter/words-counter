797aa071e5fdc928c4b502d5462e0a90a153f9ac
elise-p <elise.ponimangit@gmail.com>
2023-06-29 20:06:12 +0700

    Edit GUI to show results of words statistics

--- GUI.py
+++ GUI.py
@@ -7,10 +7,18 @@ window = tk.Tk()
 
 def getParagraph():
     content = text_box.get("1.0", tk.END)
-    return content
+    output(content)
 
-def output():
-    print(functionList.wordsCounter(getParagraph()))
+def output(words):
+    # print(functionList.wordsCounter(words))
+    result_words_countedLBL.config(text = functionList.wordsCounter(words))
+    result_vowel_countedLBL.config(text = functionList.vowelCounter(words))
+    result_character_countedLBL.config(text = functionList.characterCounter(words))
+    result_sentence_countedLBL.config(text = functionList.sentenceCounter(words))
+    result_readTime_lbl.config(text = functionList.readingTime(words))
+    result_speakTime_lbl.config(text = functionList.speakingTime(words))
+    result_mostCommonWords.config(text = functionList.mostCommonWords(words))
+
 
 window.geometry("600x500")
 window.title("Words Counter")
@@ -26,24 +34,38 @@ submit_button.place(x=150,y=410)
 
 words_countedLBL = tk.Label(window, text="words counted:")
 words_countedLBL.place(x=385, y=70)
+result_words_countedLBL = tk.Label(window)
+result_words_countedLBL.place(x=475, y=70)
 
 vowel_countetLBL = tk.Label(window, text="vowel counted: ")
 vowel_countetLBL.place(x=385, y=100)
+result_vowel_countedLBL = tk.Label(window)
+result_vowel_countedLBL.place(x = 475, y = 100)
 
 character_countedLBL = tk.Label(window, text="character counted: ")
 character_countedLBL.place(x=385, y=130)
+result_character_countedLBL = tk.Label(window)
+result_character_countedLBL.place(x=490,y=130)
 
-sentance_countedLBL = tk.Label(window, text="sentance counted: ")
-sentance_countedLBL.place(x=385, y=160)
+sentence_countedLBL = tk.Label(window, text="sentence counted: ")
+sentence_countedLBL.place(x=385, y=160)
+result_sentence_countedLBL = tk.Label(window)
+result_sentence_countedLBL.place(x=490,y=160)
 
 readTime_lbl = tk.Label(window, text="estimate read time: ")
 readTime_lbl.place(x=385, y=190)
+result_readTime_lbl = tk.Label(window)
+result_readTime_lbl.place(x=495,y=190)
 
 speakTime_lbl = tk.Label(window, text="estimate speaking time: ")
 speakTime_lbl.place(x=385, y=220)
+result_speakTime_lbl  = tk.Label(window)
+result_speakTime_lbl.place(x=520,y=220)
 
 mostCommonWords = tk.Label(window, text="Common words: ")
 mostCommonWords.place(x=385, y=250 )
+result_mostCommonWords = tk.Label(window)
+result_mostCommonWords.place(x=490,y=250)
 
 
 window.mainloop()
--- functionList.py
+++ functionList.py
@@ -1,85 +1,42 @@
-def wordsCounter(sentance: str) -> str:
-    #make new variable and split the sentance with split() function
-    listWords = sentance.split()
+def wordsCounter(sentence: str) -> str:
+    #make new variable and split the sentence with split() function
+    listWords = sentence.split()
     wordsCounted = len(listWords)
     return wordsCounted
 
-def vowelCounter(sentance: str) -> str:
+def vowelCounter(sentence: str) -> str:
     #make a list to store list of vowel words
     vowels = ["a", "e", "i", "o", "u"]
     #initiate the count program from 0
     count = 0
-    for char in sentance:
+    for char in sentence:
         if char in vowels:
             count += 1
     return count
 
-def characterCounter(sentance: str) -> str:
-    count = len(sentance)
+def characterCounter(sentence: str) -> str:
+    count = len(sentence)
     return count
 
-def sentanceCounter(sentance: str) -> str:
+def sentenceCounter(sentence: str) -> str:
     dot = '.'
     count = 0
-    for char in sentance:
+    for char in sentence:
         if char in dot:
             count += 1
     return count
 
-def readingTime(sentance: str) -> str:
-    listWords = sentance.split()
+def readingTime(sentence: str) -> str:
+    listWords = sentence.split()
     wordsCounted = len(listWords)
     readTime = round(wordsCounted / 200,3)
     return readTime
 
-def speakingTime(sentance: str) -> str:
-    listWords = sentance.split()
+def speakingTime(sentence: str) -> str:
+    listWords = sentence.split()
     wordsCounted = len(listWords)
     speakTime = round(wordsCounted / 135, 3)
     return speakTime
-
-def mostCommonWords(sentence: str):
-    #listing array of distinct words and its count
-    oriListWords = sentence.split()
-    listWords = []
-    counterWords = []
-    for word in oriListWords:
-        if word.lower() not in listWords:
-            listWords.append(word.lower())
-            counterWords.append(1)
-        else:
-            pos = listWords.index(word.lower())
-            counterWords[pos] += 1
-
-    #find unique numbers in counterWords and ordered it
-    #set in python: set elements are unique, and not ordered
-    distinctCounter = set()
-    for count in counterWords:
-        distinctCounter.add(count)
-    distinctCounterList = sorted(distinctCounter, reverse= True)
-
-    listWords0 = []
-    listWords1 = []
-    listWords2 = []
-
-    counter = 0
-    for count in counterWords:
-        if count == distinctCounterList[0]:
-            listWords0.append(listWords[counter])
-            counter += 1
-        elif count == distinctCounterList[1] :
-            listWords1.append(listWords[counter])
-            counter += 1
-        elif count == distinctCounterList[2] :
-            listWords2.append(listWords[counter])
-            counter += 1
-        else:
-            counter += 1
-            pass
-
-    print("The first most common words is/are: " + ','.join(listWords0) + " with " + str(counterWords[listWords.index(listWords0[0])])+ " words.")
-    print("The second most common words is/are: " + ','.join(listWords1) + " with " + str(counterWords[listWords.index(listWords1[0])])+ " words.")
-    print("The third most common words is/are: " + ','.join(listWords2) + " with " + str(counterWords[listWords.index(listWords2[0])])+ " words.")
 
 def mostCommonWords(sentence: str):
     #listing array of distinct words and its count
@@ -111,8 +68,8 @@ def mostCommonWords(sentence: str):
             if count == distinctCounterList[0]:
                 listWords0.append(listWords[counter])
                 counter += 1
-        
-        print("The most common words is/are: " + ','.join(listWords0) + " with " + str(counterWords[listWords.index(listWords0[0])])+ " words.")
+        mostWords = ','.join(listWords0)
+        # print("The most common words is/are: " + ','.join(listWords0) + " with " + str(counterWords[listWords.index(listWords0[0])])+ " words.")
     else: 
         counter = 0
         for count in counterWords:
@@ -128,10 +85,15 @@ def mostCommonWords(sentence: str):
             else:
                 counter += 1
                 pass
+        mostWords =  ','.join(listWords0) + "\n" + ','.join(listWords1) + "\n" + ','.join(listWords2)
 
-        print("The first most common words is/are: " + ','.join(listWords0) + " with " + str(counterWords[listWords.index(listWords0[0])])+ " words.")
-        print("The second most common words is/are: " + ','.join(listWords1) + " with " + str(counterWords[listWords.index(listWords1[0])])+ " words.")
-        print("The third most common words is/are: " + ','.join(listWords2) + " with " + str(counterWords[listWords.index(listWords2[0])])+ " words.")
+        # print("The first most common words is/are: " + ','.join(listWords0) + " with " + str(counterWords[listWords.index(listWords0[0])])+ " words.")
+        # print("The second most common words is/are: " + ','.join(listWords1) + " with " + str(counterWords[listWords.index(listWords1[0])])+ " words.")
+        # print("The third most common words is/are: " + ','.join(listWords2) + " with " + str(counterWords[listWords.index(listWords2[0])])+ " words.")
+
+        
+        return mostWords
+    
 
 def wordRange(sentence :str, minNum :int, maxNum :int):
     listWords = sentence.split()
